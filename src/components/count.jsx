004386840485284aff1813b8fca4da51ad882076
ajoysushil18 <ajoyshil412@gmail.com>
2026-04-02 14:10:30 +0600

    try to adjust all the thing

---
+++ src/components/count.jsx
@@ -0,0 +1,18 @@
+export default function Counst() {
+    return (
+        <div className="md:px-32 mt-10 lg:mt-0 bg-linear-to-r from-[#4F39F6] to-[#9514FA] py-16 flex flex-row items-center justify-center text-white gap-10 md:gap-20">
+            <div className="flex flex-col items-center justify-center gap-3">
+                <h3 className="text-4xl font-extrabold">50k+</h3>
+                <p>Active user</p>
+            </div>
+            <div className="flex flex-col items-center justify-center gap-3">
+                <h3 className="text-4xl font-extrabold">200+</h3>
+                <p>Premium Tools</p>
+            </div>
+            <div className="flex flex-col items-center justify-center gap-3">
+                <h3 className="text-4xl font-extrabold">4.9</h3>
+                <p>Rating</p>
+            </div>
+        </div>
+    )
+}
